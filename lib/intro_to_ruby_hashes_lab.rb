a59536cd9418b2beb64937161ecddbe1644d1eab
interpreted-validator-6256 <danielle.leppertsimenauer@gmail.com>
2020-11-20 22:17:32 +0000

    Automatically backed up by Learn

--- lib/intro_to_ruby_hashes_lab.rb
+++ lib/intro_to_ruby_hashes_lab.rb
@@ -7,7 +7,10 @@ def read_from_hash(hash, key)
 end
 
 def update_counting_hash(hash, key)
-  hash[key]
-  # if the provided key is not present in the hash, add it and assign it to the value of 1
+  updated_hash = hash[key]
+  if updated_hash = hash[]
+    updated_hash = hash[1]
+  if updated_hash = hash[key]
+    updated_hash = hash[key] + 1
   # if the provided key is present, increment its value by 1
 end
